7310a4bb95bf0621a310c7e1263b88ba12ed60be
Kody <157696416+kydoCode@users.noreply.github.com>
2024-09-16 12:20:50 +0200

    Fixing after code review

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,10 +1,21 @@
 const { Book, Author } = require('../models');
- 
+
 exports.createBook = async (req, res, next) => {
     try {
+        // req.body > get the data from the request body (from the form on the frontend)
         const book = await Book.create(req.body);
-        res.statut(201).json(book)
-    } catch (error) {
+        res.status(201).json(book);
+    } catch(err){
+        next(err)
+    }
+}
+
+exports.getAllBooks = async (req, res, next) => {
+    try {
+        // SELECT * FROM books LEFT JOIN authors ON books.authorId = authors.id;
+        const books = await Book.findAll({ include: Author });
+        res.status(200).json(books);
+    } catch(err){
         next(err)
     }
 }
--- src/models/book.js
+++ src/models/book.js
@@ -1,3 +1,18 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require 
+const sequelize = require('../config/database');
 
+const Book = sequelize.define('Book', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isbn: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    publishedYear: {
+        type: DataTypes.INTEGER
+    }
+})
+
+module.exports = Book;
